ca6dc8764e52b4e7728df4f0a858d4b4c01ddfcc
BrianPetro <brian@plexm.com>
2012-12-22 10:58:38 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aehalo::Application.config.secret_token = '54f8545dd2a38819bcc435a03d7c2e7bd93471164e68df6158a7c10e84ee027d937f3bb03e674a5de6f52c7fccb15558f5892f1e930eb670f6dbc572f8e64af8'
