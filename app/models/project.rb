a82a9d4cc73081766ddc5427726e05d21142e98a
BrianPetro <brian@plexm.com>
2012-12-24 15:28:03 -0500

    Add Participant row with project and user id WHEN creating a new project -set user id in form to current_user.id -nested participants in project form Added new project button to users/show page

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,8 @@
 class Project < ActiveRecord::Base
-  attr_accessible :id, :name
+  attr_accessible :id, :name, :participants_attributes
   has_many :participants
   has_many :users, :through => :participants
   has_many :blueprints
   has_many :notes
+  accepts_nested_attributes_for :participants
 end
