0aa154dc38273a550d45f8bcbd38084cb01af45a
BrianPetro <brian@plexm.com>
2013-01-05 12:26:56 -0500

    add tags to blueprints, refactor table on project main

--- app/models/blueprint.rb
+++ app/models/blueprint.rb
@@ -1,7 +1,9 @@
 class Blueprint < ActiveRecord::Base
-  attr_accessible :id, :project_id, :bpu, :title
+  attr_accessible :id, :project_id, :bpu, :title, :tag_list
   belongs_to :project
   has_many :coordinates
   has_many :notes, :through => :coordinates
 	mount_uploader :bpu, BpuUploader
+
+	acts_as_taggable
 end
