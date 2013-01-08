9b14c1a66fd28f3b7f76fc197c96b33b5c0477b5
BrianPetro <brian@plexm.com>
2013-01-08 11:35:29 -0500

    add model notes, change notes/index-sidebar,

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,7 +1,8 @@
 class Note < ActiveRecord::Base
   attr_accessible :id, :note, :project_id, :coordinates_attributes, :note_text, :tag_list
-  belongs_to :project
-  has_many :coordinates, :dependent => :destroy
+  
+	belongs_to :project # Nested under projects
+  has_many :coordinates, :dependent => :destroy # Blueprint associations handled by coordinates
   accepts_nested_attributes_for :coordinates
 
 	acts_as_taggable
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,12 +1,21 @@
 class Project < ActiveRecord::Base
   attr_accessible :id, :name, :participants_attributes
-  has_many :participants
-  has_many :users, :through => :participants, :uniq => true
+  
+	# Products of a project
   has_many :blueprints
   has_many :notes
+	has_many :participants
   accepts_nested_attributes_for :participants
+  has_many :users, :through => :participants, :uniq => true
+
+
+
+	# For Sign Seal Submit to cities
 	has_one :seal, :dependent => :destroy
 	has_one :city, :through => :seal
+	
+
+	# For Todo Lists
 	has_many :tasks
 
 end
