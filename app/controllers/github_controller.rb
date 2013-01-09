8c9105ff14af3c4899383ad0fbddbcba00e8a8c6
BrianPetro <brian@plexm.com>
2013-01-09 18:04:46 -0500

    add github vies and controllers

---
+++ app/controllers/github_controller.rb
@@ -0,0 +1,25 @@
+class GithubController < ApplicationController
+
+  def authorize
+    github = Github.new :client_id => '6aad652b8fdf192d6a09', :client_secret => '095cdc9c9337cff345e43610b334a0db0c97a196'
+    address = github.authorize_url :redirect_uri => 'http://localhost:3000', :scope => 'repo'
+    redirect_to address
+  end
+
+  def callback
+    authorization_code = params[:code]
+    token = github.get_token authorization_code
+    access_token = token.token
+  end
+
+	def index
+		@github = Github.new
+		@commit_list = @github.repos.commits.list 'brianpetro', 'aeh'
+	end
+
+	def show
+		@sha = params[:sha]
+		github = Github.new
+		@commit = github.git_data.commits.get 'brianpetro', 'aehalo', @sha
+	end
+end
