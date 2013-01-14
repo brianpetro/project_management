8eeb45b7850bd30dd76c122740cfe74430c9324c
BrianPetro <brian@plexm.com>
2013-01-14 11:10:22 -0500

    fix project not auto participating creator by changing current_user.id to ''.name in _form. minor

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,9 +4,9 @@ class Project < ActiveRecord::Base
 	# Products of a project
   has_many :blueprints
   has_many :notes
-	has_many :participants
-  accepts_nested_attributes_for :participants
+	has_many :participants, :dependent => :destroy
   has_many :users, :through => :participants, :uniq => true
+  accepts_nested_attributes_for :participants
 
 	# For Sign Seal Submit to cities
 	has_one :seal, :dependent => :destroy
