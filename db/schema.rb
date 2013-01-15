f2e2c707c21523da5681211194ed74197f406bd9
BrianPetro <brian@plexm.com>
2013-01-15 12:43:11 -0500

    state to cities migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130114170943) do
+ActiveRecord::Schema.define(:version => 20130115173706) do
 
   create_table "blueprints", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(:version => 20130114170943) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.string   "name"
+    t.string   "state"
   end
 
   create_table "coordinates", :force => true do |t|
