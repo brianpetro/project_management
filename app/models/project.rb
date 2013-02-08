425d30fdaeb96c03c8cc543dc1e6ff93f84ed979
Brian <brian@07gj-DROID.(none)>
2013-02-07 20:33:23 -0500

    accept nested attr for

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
-  attr_accessible :id, :name, :participants_attributes, :sealed
+  attr_accessible :id, :name, :participants_attributes, :sealed, :blueprints_attributes
   
 	# Products of a project
   has_many :blueprints
@@ -7,6 +7,7 @@ class Project < ActiveRecord::Base
 	has_many :participants, :dependent => :destroy
   has_many :users, :through => :participants, :uniq => true
   accepts_nested_attributes_for :participants
+  accepts_nested_attributes_for :blueprints
 
 	# For Sign Seal Submit to cities
 	has_one :seal, :dependent => :destroy
