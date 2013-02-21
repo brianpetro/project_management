2cf6899df818032c04626fc1eccfbe5fc2e6c29b
brianpetro <brian@plexm.com>
2013-02-21 15:05:57 -0500

    make members via group on modal

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -15,7 +15,7 @@ class GroupsController < ApplicationController
   # GET /groups/1.json
   def show
     @group = Group.find(params[:id])
-
+		@member = Member.new
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @group }
